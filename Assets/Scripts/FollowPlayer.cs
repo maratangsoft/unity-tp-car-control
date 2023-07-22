5c392a5bac850cfe38aed6892ecdcf791d9b3aef
maratangsoft <maratangsoft@gmail.com>
2023-07-23 00:16:41 +0900

    [add] 수업 반영

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public GameObject player; // 참조할 게임오브젝트. 이 경우엔 Vehicle
-    private Vector3 offset = new Vector3(0, 5, -7); // 오프셋 정의
+    [SerializeField] private GameObject player; // 참조할 게임오브젝트. 이 경우엔 Vehicle
+    private Vector3 offset = new Vector3(0, 2.53f, 3.23f); // 오프셋 정의
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20.0f;
-    private float turnSpeed = 60.0f;
+    [SerializeField] private GameObject centerOfMass;
+    //[SerializeField] private float speed = 20.0f;
+    [SerializeField] private float horsePower = 150000;
+    [SerializeField] private float turnSpeed = 60.0f;
+    [SerializeField] private TextMeshProUGUI speedometerText;
+    [SerializeField] private TextMeshProUGUI rpmText;
+    [SerializeField] private List<WheelCollider> allWheels;
+
+    private float speed;
+    private float rpm;
+
     private float horizontalInput;
     private float verticalInput;
+    
+    private Rigidbody playerRb;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // Input클래스의 입력 메서드들은 생성자 및 Start()에서 호출 불가. 
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        // verticalInput을 받아서 전진
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
-        // horizontalInput을 받아서 시계방향으로 회전
-        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput); 
+        if (IsOnGround())
+        {
+            // verticalInput을 받아서 전진
+            //transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+            playerRb.AddRelativeForce(horsePower * verticalInput * Vector3.forward);
+
+            // horizontalInput을 받아서 시계방향으로 회전
+            transform.Rotate(horizontalInput * Time.deltaTime * turnSpeed * Vector3.up);
+
+            // 3.6 for kph, 2.237 for mph
+            speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 2.237f);
+            speedometerText.SetText("Speed: " + speed + " mph");
+
+            rpm = Mathf.Round((speed % 30) * 40);
+            rpmText.SetText("RPM: " + rpm);
+        }
+    }
+
+    bool IsOnGround()
+    {
+        int wheelsOnGround = 0;
+        foreach (var wheelCollider in allWheels)
+        {
+            if (wheelCollider.isGrounded) wheelsOnGround++;
+        }
+        if (wheelsOnGround == 4) return true;
+        else return false;
     }
 }
